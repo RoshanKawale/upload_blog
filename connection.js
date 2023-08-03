1c307b06301aec72588cd0fef0ef4548124e24dc
Roshan <roshankawale43@gmail.com>
2023-08-04 00:52:16 +0530

    change commit

--- connection.js
+++ connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
+
+const MONGO_URI = "mongodb+srv://roshankawale2003:nN2ZI5PRRWYnF0Ci@practice01.dpnonyf.mongodb.net/blogs?retryWrites=true&w=majority"
 
 const connectDb = async () =>{
-   const connetions  = await mongoose.connect(process.env.MONGO_URI)
+   const connetions  = await mongoose.connect(MONGO_URI)
    if(connetions) console.log("Database connected");
    else console.log("Database not connected");
 }
