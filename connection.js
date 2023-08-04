622f0800637cfada94d9be6585573cf6443e2db8
Roshan <roshankawale43@gmail.com>
2023-08-04 10:20:59 +0530

    second commit

--- connection.js
+++ connection.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 const connectDb = async () =>{
-   const connetions  = await mongoose.connect(process.env.MONGO_URI)
+   const connetions  = await mongoose.connect(process.env.MONGO_URI,{
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
    if(connetions) console.log("Database connected");
    else console.log("Database not connected");
 }
